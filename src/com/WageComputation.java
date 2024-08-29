7e6b4b81a88d6f97cb5cdc504fbbeeeb388494d0
Shivani1020 <shivani.gurnalli@gmail.com>
2024-08-29 16:40:18 +0530

    UC1 Commit

--- src/com/WageComputation.java
+++ src/com/WageComputation.java
@@ -1,11 +1,25 @@
 package com;
 
+import java.util.Random;
+
 public class WageComputation {
 	
 	public static void main(String[] args) {
 		System.out.println("--------------------------------------------------------------");
 		System.out.println("~~~~~~~~~Welcome to Employee Wage Computation Program~~~~~~~~~");
 		System.out.println("--------------------------------------------------------------");
+		
+		Random random = new Random();
+		int attendence = random.nextInt(2);   // 2 indicates that either 0 or 1 is generated. 
+		if(attendence == 1)  // 1 -> Present, 0-> Absent
+		{
+			System.out.println("Employee is Present.");
+		}
+		else 
+		{
+			System.out.println("Employee is Absent.");
+		}
+		
 	}
 
 }
