90ddbefa0fd2bf10a075014fdfa596e0876acaee
Shivani1020 <shivani.gurnalli@gmail.com>
2024-08-29 15:45:26 +0530

    first commit

---
+++ src/com/WageComputation.java
@@ -0,0 +1,11 @@
+package com;
+
+public class WageComputation {
+	
+	public static void main(String[] args) {
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("~~~~~~~~~Welcome to Employee Wage Computation Program~~~~~~~~~");
+		System.out.println("--------------------------------------------------------------");
+	}
+
+}
