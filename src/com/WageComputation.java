c6ad866fd3dcc1ce6c51cd697425423013709fa1
Shivani1020 <shivani.gurnalli@gmail.com>
2024-08-30 07:30:12 +0530

    UC3 Commit

--- src/com/WageComputation.java
+++ src/com/WageComputation.java
@@ -3,16 +3,18 @@ package com;
 import java.util.Random;
 
 public class WageComputation {
+	
 	static final int wage_per_hour = 20;
 	static final int full_day_hour = 8;
+	static final int part_time_wage = 4;
 	
 	static int getAttendence(){
 		Random random = new Random();
-		return random.nextInt(2);
+		return random.nextInt(3);   //0-> Absent, 1-> Full time , 2-> Part time
 	}
 	
-	static int calculateDailyWage() {
-		return full_day_hour * wage_per_hour;
+	static int calculateDailyWage(int hours) {
+		return hours * wage_per_hour;
 	}
 	
 	public static void main(String[] args) {
@@ -22,15 +24,21 @@ public class WageComputation {
 		
 		int attendence = getAttendence();  // 2 indicates that either 0 or 1 is generated.
 		 
-		if(attendence == 1)  // 1 -> Present, 0-> Absent
+		if(attendence == 1)  //  1-> Full time
 		{
-			System.out.println("Employee is Present.");
-			int dailywage = calculateDailyWage();
-			System.out.println("Daily wage of Employee: "+dailywage);
+			System.out.println("Employee is Present for Full time.");
+			int dailywage = calculateDailyWage(full_day_hour);
+			System.out.println("Daily wage of Full Time Employee: "+dailywage);
 		}
-		else 
+		else if(attendence == 2)  //2-> Part time
 		{
-			System.out.println("Employee is Absent.");			
+			System.out.println("Employee is present for Part time");
+			int dailywage = calculateDailyWage(part_time_wage);
+			System.out.println("Daily wage of Part time Employee: "+dailywage);
+		}
+		else   // 0-> Absent
+		{
+			System.out.println("Employee is Absent.");
 		}
 		
 		
