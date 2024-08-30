f3c9f0f51b11bb7588ddbaa1141fd0a62ea4caff
Shivani1020 <shivani.gurnalli@gmail.com>
2024-08-30 12:06:37 +0530

    UC8 Commit

--- src/com/WageComputation.java
+++ src/com/WageComputation.java
@@ -6,26 +6,33 @@ import java.util.Random;
 
 public class WageComputation {
 	
-	static final int wage_per_hour = 20;
-	static final int full_day_hour = 8;
-	static final int part_time_hour = 4;
-	static final int working_day_per_month = 20;
-	static final int max_working_day = 20;
-	static final int max_working_hour = 100;
+//	static final int wage_per_hour = 20;
+//	static final int full_day_hour = 8;
+//	static final int part_time_hour = 4;
+//	static final int working_day_per_month = 20;
+//	static final int max_working_day = 20;
+//	static final int max_working_hour = 100;
 	
-	private int total_wage;
-    private int totalWorkingDays;
-    private int totalWorkingHours;
+//	private int total_wage;
+//    private int totalWorkingDays;
+//    private int totalWorkingHours;
     
-    public WageComputation() // constructor to initialize class variables
-    {
-    	this.total_wage = 0;
-    	this.totalWorkingDays = 0;
-    	this.totalWorkingHours = 0;
-    }
+//    public WageComputation() // constructor to initialize class variables
+//    {
+//    	this.total_wage = 0;
+//    	this.totalWorkingDays = 0;
+//    	this.totalWorkingHours = 0;
+//    }
     
-    public void computeEmployeeWage()   // method to calculate employee wage
+    public static void computeEmployeeWage(String company, int wage_per_hour, int max_working_day, int max_working_hour)   
     {
+    	int total_wage = 0;
+    	int totalWorkingDays = 0;
+    	int totalWorkingHours = 0;
+    	final int full_day_hour = 8;
+    	final int part_time_hour = 4;
+    	System.out.println("\t\tWage Computation For "+company);
+    	System.out.println("\t--------------------------------------------------------------");
     	while(totalWorkingDays < max_working_day && totalWorkingHours < max_working_hour)
 		{
 			int attendence = getAttendence();
@@ -38,7 +45,7 @@ public class WageComputation {
 			case 1:
 			{
 				workedhours = full_day_hour;
-				dailywage = calculateDailyWage(workedhours);
+				dailywage = calculateDailyWage(workedhours, wage_per_hour);
 				System.out.println("\tDay "+totalWorkingDays+": Employee present for Full-time and wage for the day: $"+dailywage );
 				
 			}
@@ -47,7 +54,7 @@ public class WageComputation {
 			case 2:
 			{
 				workedhours = part_time_hour;
-				dailywage = calculateDailyWage(workedhours);
+				dailywage = calculateDailyWage(workedhours, wage_per_hour);
 				System.out.println("\tDay "+totalWorkingDays+": Employee present for Part-time and wage for the day: $"+dailywage );
 				
 			}
@@ -62,7 +69,13 @@ public class WageComputation {
 			total_wage += dailywage;
 			totalWorkingHours += workedhours; 
 		}
-    	display();
+    	//display
+    	System.out.println();
+    	System.out.println("\tTotal wage for "+company+ ": $"+total_wage);
+    	System.out.println("\tTotal Working Days for "+company+": "+ totalWorkingDays);
+    	System.out.println("\tTotal Working Hours for "+company+ ": "+totalWorkingHours);
+    	System.out.println("\t--------------------------------------------------------------");
+    	System.out.println();
     }
 	
 	static int getAttendence(){
@@ -70,28 +83,21 @@ public class WageComputation {
 		return random.nextInt(3);   //0-> Absent, 1-> Full time , 2-> Part time
 	}
 	
-	static int calculateDailyWage(int hours) {
+	static int calculateDailyWage(int hours, int wage_per_hour ) {
 		return hours * wage_per_hour;
 	}
-	
-	public void display() {
-		System.out.println();
-		System.out.println("\tTotal wage of Employee for one month: $"+total_wage);
-		System.out.println("\tTotal Working Days: "+ totalWorkingDays);
-		System.out.println("\tTotal Working Hour: "+totalWorkingHours);
-	}
+
 	
 	public static void main(String[] args) {
 		System.out.println("\t--------------------------------------------------------------");
 		System.out.println("\t~~~~~~~~~Welcome to Employee Wage Computation Program~~~~~~~~~");
 		System.out.println("\t--------------------------------------------------------------");
+		System.out.println();
 		
-		WageComputation employee = new WageComputation();
-		employee.computeEmployeeWage();
-		
-//		int total_wage = 0;
-//		int totalWorkingDays = 0;
-//		int totalWorkingHours = 0;
+		computeEmployeeWage("Company Abc", 20, 20, 100);
+		computeEmployeeWage("Company DEF", 25, 30, 150);
+		computeEmployeeWage("Company GHI", 20, 25, 120);
+
 	}
 
 }
