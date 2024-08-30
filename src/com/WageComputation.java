307e2ba1758e262586081d061b19ce73927f19d2
Shivani1020 <shivani.gurnalli@gmail.com>
2024-08-30 10:27:48 +0530

    UC5 Commit

--- src/com/WageComputation.java
+++ src/com/WageComputation.java
@@ -7,6 +7,7 @@ public class WageComputation {
 	static final int wage_per_hour = 20;
 	static final int full_day_hour = 8;
 	static final int part_time_wage = 4;
+	static final int working_day_per_month = 20;
 	
 	static int getAttendence(){
 		Random random = new Random();
@@ -21,31 +22,45 @@ public class WageComputation {
 		System.out.println("--------------------------------------------------------------");
 		System.out.println("~~~~~~~~~Welcome to Employee Wage Computation Program~~~~~~~~~");
 		System.out.println("--------------------------------------------------------------");
-		int dailywage =0;
-		int attendence = getAttendence();  // 2 indicates that either 0 or 1 is generated.
-		switch(attendence)
-		{
-		case 1:
-		{
-			System.out.println("Employee is Present for Full time.");
-			dailywage = calculateDailyWage(full_day_hour);
-			
-		}
-		break;
+		int total_wage = 0;
 		
-		case 2:
-		{
-			System.out.println("Employee is present for Part time");
-			dailywage = calculateDailyWage(part_time_wage);
-		}
-		break;
 		
-		case 0:
-			System.out.println("Employee is Absent.");
+		for(int day=1; day<=working_day_per_month; day++)
+		{
+			int dailywage =0;
+			int attendence = getAttendence();  // 2 indicates that either 0 or 1 is generated.
+			switch(attendence)
+			{
+			case 1:
+			{
+				//System.out.println("Employee is Present for Full time.");
+				dailywage = calculateDailyWage(full_day_hour);
+				System.out.println("Day "+day+": Employee present for Full-time and wage for the day: $"+dailywage );
+				
+				
+			}
 			break;
+			
+			case 2:
+			{
+				//System.out.println("Employee is present for Part time");
+				dailywage = calculateDailyWage(part_time_wage);
+				System.out.println("Day "+day+": Employee present for Part-time and wage for the day: $"+dailywage );
+				
+			}
+			break;
+			
+			case 0:
+				//System.out.println("Employee is Absent.");
+				System.out.println("Day "+day+": Employee Absent and wage for the day: $0" );
+				
+				break;
+			}
+			total_wage += dailywage;
 		}
 		
-		System.out.println("Daily wage of Employee: $"+dailywage);
+		System.out.println();
+		System.out.println("Total wage of Employee for one month: $"+total_wage);
 		
 	}
 
